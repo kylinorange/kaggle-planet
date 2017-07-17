b77b82d545b584fc9982cddc0c5aec10b548635e
You Jia <jiayou50@gmail.com>
2017-07-16 17:36:43 -0700

    v11 - inception v3

--- utils/models.py
+++ utils/models.py
@@ -14,6 +14,7 @@ from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 from keras.layers import Input
 from keras.models import Model
+from keras.applications.inception_v3 import InceptionV3
 
 from resnet50 import ResNet50
 from leaky_resnet50 import LeakyResNet50
@@ -48,7 +49,6 @@ class Models:
         x = base_model.output
         predictions = Dense(17, activation='sigmoid')(x)
 
-        # this is the model we will train
         model = Model(inputs=base_model.input, outputs=predictions)
 
         # first: train only the top layers (which were randomly initialized)
@@ -66,6 +66,26 @@ class Models:
     @staticmethod
     def load_resnet50(path, input_shape=(256, 256, 3), leaky=False):
         model = Models.new_resnet50(input_shape, leaky)
+        model.load_weights(path)
+        return model
+
+    @staticmethod
+    def new_incnet(input_shape=(256, 256, 3)):
+        base_model = InceptionV3(weights=None, include_top=False, pooling='avg', input_tensor = Input(shape=input_shape))
+
+        x = base_model.output
+        predictions = Dense(17, activation='sigmoid')(x)
+
+        model = Model(inputs=base_model.input, outputs=predictions)
+
+        model.compile(metrics=[amazon_score, 'accuracy'],
+                      loss='binary_crossentropy',
+                      optimizer=Adam(lr=0.001))
+        return model
+
+    @staticmethod
+    def load_incnet(path, input_shape=(256, 256, 3)):
+        model = Models.new_incnet(input_shape)
         model.load_weights(path)
         return model
 
